131d8134e864779fe2dce6eb6c7d5794b3c07009
akshch <akshaych2786@gmail.com>
2023-06-23 16:05:07 +0530

    added active storage to save attachment

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,6 @@
 module ProductsHelper
+
+  def product_image(product)
+    product.image.presence ||  "tomato.jpg"
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
 
   has_many :line_items, dependent: :destroy
+  has_one_attached :image
 end
