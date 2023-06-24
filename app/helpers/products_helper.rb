9f8f0ccadd3d9b96c208242c9cdf4bd031664cac
akshch <akshaych2786@gmail.com>
2023-06-24 19:17:22 +0530

    product and card show page changes

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,4 +3,8 @@ module ProductsHelper
   def product_image(product)
     product.image.presence ||  "tomato.jpg"
   end
+
+  def get_product_description(product)
+    product.description.presence || 'Some quick example text to build on the card title and make up the bulk of the cards content.'
+  end
 end
